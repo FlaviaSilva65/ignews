1e6d3475a5bbfa114c806b11659fc15e83fe7f84
Flavia Silva <flaviaass65@gmail.com>
2022-10-25 17:08:04 -0300

    Produto & Checkout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,21 @@
 import { useRouter } from 'next/router';
 import { json } from 'node:stream/consumers';
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
 
 export default function Product(){
     const { query } = useRouter()
     
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit error eaque est. Ratione assumenda commodi tempore repudiandae harum alias, optio fugit, eligendi consectetur blanditiis, quam earum omnis impedit error aperiam.</p>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
